cfbdf94465bdeb9d139d39d94a610a8a1708edca
Friedrich Bachinger <friedrich.bachinger@codefluss.at>
2023-04-04 19:01:46 +0200

    added routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import DirectoryContainer from './components/directory/directory.component';
-import Home from './routes/home/home.component';
 import { Routes, Route } from 'react-router-dom';
+
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+
+
+const Shop = () => {
+    return (
+        <div>
+            <p>I am the navigation bar</p>
+        </div>
+    )
+}
 
 const App = () => {
   return (
     <Routes>
-        <Route path="/" element={<Home />}/>
+        <Route path="/" element={<Navigation />}>
+            <Route index element={<Home />} />
+            <Route path="/shop" element={<Shop />} />
+        </Route>
     </Routes>
   );
 }
--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,4 +1,5 @@
 import DirectoryContainer from '../../components/directory/directory.component';
+import { Outlet } from 'react-router-dom';
 
 const Home = () => {
     const categories = [

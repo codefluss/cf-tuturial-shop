279096081c08ee83b727ea5d364a69e94ad214e9
Friedrich Bachinger <friedrich.bachinger@codefluss.at>
2023-04-10 22:58:46 +0200

    added redux persist

--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,8 @@ import { compose, createStore,  applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import { rootReducer } from './root.reducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 const loggerMiddleWare = (store) => (next) => (action) => {
   if (!action.type) {
@@ -17,7 +19,17 @@ const loggerMiddleWare = (store) => (next) => (action) => {
   return next(action);
 }
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['user']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const middleWares= [logger];
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer,  undefined, composeWithDevTools(composedEnhancers));
+export const store = createStore(persistedReducer,  undefined, composeWithDevTools(composedEnhancers));
+
+export const persistor = persistStore(store);

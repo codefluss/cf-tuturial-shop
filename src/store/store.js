860b9395ffbf58344ff0b52ac6a6a33f2aa76c36
Friedrich Bachinger <friedrich.bachinger@codefluss.at>
2023-04-10 09:35:10 +0200

    adding middleware

--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,20 @@ import logger from 'redux-logger';
 import { rootReducer } from './root.reducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
+const loggerMiddleWare = (store) => (next) => (action) => {
+  if (!action.type) {
+    return next(action);
+  }
+  
+  console.log('type: ', action.type);
+  console.log('action: ', action.payload);
+  console.log('current state: ', store.getState());
+  
+  next(action);
+  console.log('next state: ', store.getState());
+  return next(action);
+}
+
 const middleWares= [logger];
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
